17949bf9e1c8498559b06bb47ec4b0ee149c9076
Sujit Kumar <121103932+isujitkr@users.noreply.github.com>
2024-01-20 00:34:56 +0530

    Time: 7 ms (77.88%), Space: 45.1 MB (88.85%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,24 +1,44 @@
 class Solution {
     public int minSteps(String s, String t) {
-        if (s.length() != t.length()) {
-            throw new IllegalArgumentException("Input strings must have the same length");
+        // if (s.length() != t.length()) {
+        //     throw new IllegalArgumentException("Input strings must have the same length");
+        // }
+
+        // Map<Character, Integer> charCount = new HashMap<>();
+
+        // for (char c : s.toCharArray()) {
+        //     charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        // }
+
+        // for (char c : t.toCharArray()) {
+        //     charCount.put(c, charCount.getOrDefault(c, 0) - 1);
+        // }
+
+        // int steps = 0;
+        // for (int count : charCount.values()) {
+        //     steps += Math.abs(count);
+        // }
+
+        // return steps / 2;
+
+        int arr[] = new int[26];
+        int arr1[] = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            arr[ch-'a']++;
         }
 
-        Map<Character, Integer> charCount = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        for (char ch : t.toCharArray()) {
+            arr1[ch-'a']++;
         }
 
-        for (char c : t.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) - 1);
-        }
+        int ans = 0;
 
-        int steps = 0;
-        for (int count : charCount.values()) {
-            steps += Math.abs(count);
+        for (int i = 0; i < 26; i++) {
+            ans += Math.abs(arr[i] - arr1[i]);
         }
-
-        return steps / 2;
+        
+        return ans/2;
+    
     }
 }
