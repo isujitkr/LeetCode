f32ceb7617d91e2a608bcf0cdb283329cc245329
Sujit Kumar <121103932+isujitkr@users.noreply.github.com>
2024-04-15 13:39:38 +0530

    Time: 1 ms (99.96%), Space: 49.1 MB (9.16%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,39 +13,56 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode tempA = headA;
         ListNode tempB = headB;
-        int sizeA = 0;
-        int sizeB = 0;
-        while(tempA != null){
-            sizeA++;
-            tempA = tempA.next;
-        }
 
-        while(tempB != null){
-            sizeB++;
-            tempB = tempB.next;
-        }
+        //1st approach
 
-        tempA = headA;
-        tempB = headB;
-        int idx = Math.abs(sizeA - sizeB);
+        // int sizeA = 0;
+        // int sizeB = 0;
+        // while(tempA != null){
+        //     sizeA++;
+        //     tempA = tempA.next;
+        // }
+
+        // while(tempB != null){
+        //     sizeB++;
+        //     tempB = tempB.next;
+        // }
+
+        // tempA = headA;
+        // tempB = headB;
+        // int idx = Math.abs(sizeA - sizeB);
         
-        if(sizeA > sizeB){
-            while(idx > 0){
-                tempA = tempA.next;
-                idx--;
-            }
-        }
-        else{
-            while(idx > 0){
-                tempB = tempB.next;
-                idx--;
-            }
+        // if(sizeA > sizeB){
+        //     while(idx > 0){
+        //         tempA = tempA.next;
+        //         idx--;
+        //     }
+        // }
+        // else{
+        //     while(idx > 0){
+        //         tempB = tempB.next;
+        //         idx--;
+        //     }
             
-        }
+        // }
 
+        // while(tempA != tempB){
+        //     tempA = tempA.next;
+        //     tempB = tempB.next;
+        // }
+
+        // return tempA;
+
+        //2nd approach
         while(tempA != tempB){
             tempA = tempA.next;
             tempB = tempB.next;
+
+            if(tempA == tempB) return tempA;
+
+            if(tempA == null) tempA = headB;
+
+            if(tempB == null) tempB = headA;
         }
 
         return tempA;
